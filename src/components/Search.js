9f2cab1c13e10d946fbb55bffbebf58c413910fe
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-23 15:23:42 +0200

    Add searchbar

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import "./search.css";
+import { createElement } from "../utils/elements";
+
+function Search({ onchange }) {
+  const input = createElement("input", {
+    placeholder: "Search for houses",
+  });
+  const button = createElement("button", {
+    innerText: "🔍",
+  });
+  const form = createElement("form", {
+    className: "search",
+    children: [input, button],
+    onsubmit: (event) => {
+      event.preventDefault();
+      onchange(input.value);
+    },
+  });
+  return form;
+}
+
+export default Search;
