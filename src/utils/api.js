8857851fb53dce8ea68af6c038d446dd3232d196
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-23 15:54:09 +0200

    Add filter funktion

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,7 @@ export async function getAllHouses(name) {
   let url = `https://www.anapioficeandfire.com/api/houses/`;
   if (name) {
     url += `?name=${name}`;
+    console.log(url);
   }
   const response = await fetch(url);
   const data = await response.json();
