56c17c66ca921e3aa57e211bc2b7a4b1c1599526
Matt-1985 <blatsche@web.de>
2020-10-23 11:28:41 +0200

    Add basic structure

---
+++ src/components/House.js
@@ -0,0 +1,30 @@
+import "./house.css";
+import { createElement } from "../utils/elements";
+
+function createHouseCard() {
+  const houseTitle = createElement("h3", {
+    className: "house__title",
+  });
+
+  const houseQuote = createElement("blockquote", {
+    className: "house__quote",
+  });
+
+  const houseInfo = createElement("div", {
+    className: "house__Info",
+    children: [
+      createElement("p", {
+        innerText: "Region",
+      }),
+    ],
+  });
+  const houseContainer = createElement("div", {
+    //evtl article einsetzen
+    className: "house__container",
+    children: [houseTitle, houseQuote, houseInfo],
+  });
+
+  return houseContainer;
+}
+
+export default createHouseCard;
